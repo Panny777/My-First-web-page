b7a60233891c159e61694ba2c1fcd0ce82bb36e4
Panny777 <59440304+Panny777@users.noreply.github.com>
2020-04-06 17:24:19 +0300

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <title>Joyapk</title>
+    </head>
+    <body>
+    <h2>What Can JavaScript Do?</h2>
+
+    </body>
+      
+
+  
+</html>
+
+
+
---
+++ script.js
@@ -0,0 +1,17 @@
+
+function plus(){
+    document.getElementById("demo").innerHTML =10 + 210;
+}
+
+function minus(){
+    document.getElementById("demo").innerHTML =10 - 210;
+}
+
+function divide(){
+    document.getElementById("demo").innerHTML =10 / 210;
+}
+
+function multiply(){
+    document.getElementById("demo").innerHTML =10 * 210;
+}
+
